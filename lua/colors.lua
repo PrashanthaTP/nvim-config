6e9cec1ee5e426c250de2b5b4ec1a5584b7059a0
PrashanthaTP <tpprashantha@gmail.com>
2022-07-31 17:15:41 +0530

    Feat(Colors) set transparent bg for the end of buffer

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,5 +1,4 @@
 vim.cmd("colorscheme github")
-vim.cmd("highlight! Normal cterm=NONE gui=NONE ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
 local function highlight(group,fg,bg)
 --print("highlight! "..group.." guifg='"..fg.."' guibg='"..bg.."'")
 vim.cmd("highlight! "..group.." guifg='"..fg.."' guibg='"..bg.."'")
@@ -11,5 +10,10 @@ highlight("LineNr","#3D3F5F","#061018")
 highlight("SignColumn","#3D3F5F","#061018")
 highlight("CursorLine","NONE","#081A1A")
 highlight("CursorLineNr","#3D3F5F","#0A1D1F")
+
+-- To have transparent background at the end of buffer
+-- Reference : https://stackoverflow.com/questions/3725526/how-do-i-change-the-character-vim-uses-to-number-blank-lines-in-a-buffer
+vim.cmd("highlight! Normal cterm=NONE gui=NONE ctermfg=NONE ctermbg=NONE guifg=NONE guibg=NONE")
+highlight("EndOfBuffer","NONE","NONE")
 
 
