31d6f974c3524791e9777cab39569d891d4f3a92
PrashanthaTP <tpprashantha@gmail.com>
2023-02-01 22:12:09 +0530

    Set(Keymap) Add shortcut to format file using lsp

--- lua/tpp/remap.lua
+++ lua/tpp/remap.lua
@@ -1,7 +1,15 @@
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>pv",vim.cmd.Ex,{})
-vim.keymap.set("i","jk","<ESC>",{})
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
+vim.keymap.set("i", "jk", "<ESC>", {})
 vim.keymap.set('n', '<Leader>vs', ':source $MYVIMRC<CR>',
-                        {noremap = true})
+    { noremap = true })
 vim.keymap.set('n', '<Leader>vo', ':edit $MYVIMRC<CR>',
-                        {noremap = true})
+    { noremap = true })
+
+-- https://jdhao.github.io/2022/10/05/nvim-v08-release/#check-command-or-map-location
+vim.keymap.set('n', '<Leader>ff', "", {
+    desc = "format file using lsp",
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
