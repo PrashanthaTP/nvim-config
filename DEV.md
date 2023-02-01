7645395bf592e48a8935fac90490b5b45dee6924
PrashanthaTP <tpprashantha@gmail.com>
2023-02-01 22:13:02 +0530

    Fix(Lsp,#4) Add Mason plugin bin path,clangd path to PATH

---
+++ DEV.md
@@ -0,0 +1,5 @@
+# Dev
+
+## Lua mapping
+
++ [Jdhao's Blog: Lua Mapping via callback](https://jdhao.github.io/2022/10/05/nvim-v08-release/#check-command-or-map-location)
--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
 -- TPP Neovim Configuration
+
+MASON_BIN_PATH = "D:/Applications/Nvim/nvim/mason/bin"
+CLANGD_BIN_PATH = "D:/Applications/Nvim/nvim/mason/packages/clangd/clangd/bin"
+
+local join = function(...)
+    local arg = { ... }
+    local res = ""
+    for _, item in ipairs(arg) do
+        res = res .. tostring(item)
+    end
+    return res
+end
+
+local pathEntrySeperator = ";"
+vim.env.PATH = join(MASON_BIN_PATH, pathEntrySeperator) .. vim.env.PATH
+vim.env.PATH = join(CLANGD_BIN_PATH, pathEntrySeperator) .. vim.env.PATH
+
 require('tpp')
